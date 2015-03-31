822f79d40f7401839eea095a1e0c81b477ae9618
Martin van Wingerden <martin.van.wingerden@diginh.nl>
2015-03-31 11:30:52 +0200

    Add the files needed for setup to repo

---
+++ setup.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+import bootstrap3
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+version = bootstrap3.__version__
+
+if sys.argv[-1] == 'publish':
+    os.system('cd docs && make html')
+    os.system('python setup.py sdist upload')
+    print("You probably want to also tag the version now:")
+    print("  git tag -a %s -m 'version %s'" % (version, version))
+    print("  git push --tags")
+    sys.exit()
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read().replace('.. :changelog:', '')
+
+setup(
+    name='django-filechooser',
+    version=version,
+    description="""jQuery Filechooser for Django projects""",
+    long_description=readme + '\n\n' + history,
+    author='Martin van Wingerden',
+    author_email='martinvw@gmail.com',
+    url='https://github.com/martinvw/django-filechooser',
+    packages=[
+        'filechooser',
+    ],
+    include_package_data=True,
+    install_requires=[
+    ],
+    license="MIT",
+    zip_safe=False,
+    keywords='django-filechooser',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Utilities',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+    ],
+)
