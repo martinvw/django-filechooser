17b0e30d9b8f09daa9dab8f0b4d5a8323d38641f
Martin van Wingerden <martin.van.wingerden@diginh.nl>
2015-04-01 09:21:06 +0200

    Added manage.py and testsettings

---
+++ testsettings.py
@@ -0,0 +1,17 @@
+import django.conf.global_settings as DEFAULT_SETTINGS
+
+
+SECRET_KEY = 'test-secret-key'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    },
+}
+
+INSTALLED_APPS = (
+    'filechooser',
+)
+
+MIDDLEWARE_CLASSES = DEFAULT_SETTINGS.MIDDLEWARE_CLASSES
